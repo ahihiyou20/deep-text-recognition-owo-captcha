653cb4b4b9749ec7a7fd53d6f0af03b10de09699
ahihiyou20 <95456402+ahihiyou20@users.noreply.github.com>
2024-06-23 19:46:42 +0700

    Update test.py

--- test.py
+++ test.py
@@ -252,7 +252,7 @@ if __name__ == '__main__':
     parser.add_argument('--saved_model', required=True, help="path to saved_model to evaluation")
     """ Data processing """
     parser.add_argument('--batch_max_length', type=int, default=6, help='maximum-label-length')
-    parser.add_argument('--imgH', type=int, default=60, help='the height of the input image')
+    parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
     parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
     parser.add_argument('--rgb', action='store_true', default=True, help='use rgb input')
     parser.add_argument('--character', type=str, default='abcdefghijklmnopqrstuvwxyz', help='character label')
