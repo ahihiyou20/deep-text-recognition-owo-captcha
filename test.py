d5bd4eb2c2fbce850446ed517292d9bda1847234
ahihiyou20 <95456402+ahihiyou20@users.noreply.github.com>
2024-07-06 14:24:41 +0700

    Update test.py

--- test.py
+++ test.py
@@ -265,9 +265,9 @@ if __name__ == '__main__':
     parser.add_argument('--FeatureExtraction', type=str, default='ResNet', help='FeatureExtraction stage. VGG|RCNN|ResNet')
     parser.add_argument('--SequenceModeling', type=str, default='BiLSTM', help='SequenceModeling stage. None|BiLSTM')
     parser.add_argument('--Prediction', type=str, default='Attn', help='Prediction stage. CTC|Attn')
-    parser.add_argument('--num_fiducial', type=int, default=30, help='number of fiducial points of TPS-STN')
+    parser.add_argument('--num_fiducial', type=int, default=40, help='number of fiducial points of TPS-STN')
     parser.add_argument('--input_channel', type=int, default=1, help='the number of input channel of Feature extractor')
-    parser.add_argument('--output_channel', type=int, default=512,
+    parser.add_argument('--output_channel', type=int, default=1024,
                         help='the number of output channel of Feature extractor')
     parser.add_argument('--hidden_size', type=int, default=512, help='the size of the LSTM hidden state')
 
