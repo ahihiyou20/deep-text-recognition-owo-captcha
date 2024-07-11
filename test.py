94e140fc7a4c06fb53fa520f20225c115ed5230f
ahihiyou20 <95456402+ahihiyou20@users.noreply.github.com>
2024-07-11 22:02:05 +0700

    Update test.py

--- test.py
+++ test.py
@@ -258,13 +258,13 @@ if __name__ == '__main__':
     parser.add_argument('--character', type=str, default='abcdefghijklmnopqrstuvwxyz', help='character label')
     parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
     parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
-    parser.add_argument('--data_filtering_off', action='store_true', help='for data_filtering_off mode')
+    parser.add_argument('--data_filtering_off', default=True, action='store_true', help='for data_filtering_off mode')
     parser.add_argument('--baiduCTC', action='store_true', help='for data_filtering_off mode')
     """ Model Architecture """
     parser.add_argument('--Transformation', type=str, default='None', help='Transformation stage. None|TPS')
-    parser.add_argument('--FeatureExtraction', type=str, default='RCNN', help='FeatureExtraction stage. VGG|RCNN|ResNet')
-    parser.add_argument('--SequenceModeling', type=str, default='BiLSTM', help='SequenceModeling stage. None|BiLSTM')
-    parser.add_argument('--Prediction', type=str, default='Attn', help='Prediction stage. CTC|Attn')
+    parser.add_argument('--FeatureExtraction', type=str, default='Resnet', help='FeatureExtraction stage. VGG|RCNN|ResNet')
+    parser.add_argument('--SequenceModeling', type=str, default='None', help='SequenceModeling stage. None|BiLSTM')
+    parser.add_argument('--Prediction', type=str, default='CTC', help='Prediction stage. CTC|Attn')
     parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
     parser.add_argument('--input_channel', type=int, default=1, help='the number of input channel of Feature extractor')
     parser.add_argument('--output_channel', type=int, default=512,
